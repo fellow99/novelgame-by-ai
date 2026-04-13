110e41c585c70b1572affac6a6f7777812ad84b0
fellow99 <fellow99@163.com>
2026-04-13 22:22:32 +0800

    feat: 上下文路径改为/novelgame-by-ai

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/novelgame/',
+  base: '/novelgame-by-ai/',
   plugins: [vue()],
   resolve: {
     alias: {
